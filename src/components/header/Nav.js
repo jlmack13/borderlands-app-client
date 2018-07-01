757d233380121c25cf1d990d392cd7908901f6f0
jlmack13 <jennimacklin@gmail.com>
2018-07-01 12:46:58 -0400

    changed background color of active navlink

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -8,9 +8,9 @@ const Nav = () => {
     return (
         <div className="nav">
             <ul className="nav-group">
-                <NavLink to="/" exact activeStyle={{background: 'chartreuse'}}>Home</NavLink>
-                <NavLink to="/about" exact activeStyle={{background: 'chartreuse'}}>About</NavLink>
-                <NavLink to="/storymode" exact activeStyle={{background: 'chartreuse'}}>Story Mode</NavLink>
+                <NavLink to="/" exact activeStyle={{background: '#434E7B'}}>Home</NavLink>
+                <NavLink to="/about" exact activeStyle={{background: '#434E7B'}}>About</NavLink>
+                <NavLink to="/storymode" exact activeStyle={{background: '#434E7B'}}>Story Mode</NavLink>
                 <Dropdown name="Manufacturer" sub_items={["Anshin", "Bandit", "Dahl", "Hyperion", "Jakobs", "Maliwan", "Pangolin", "Tediore", "Torgue", "Vladof"]} />
                 <Dropdown name="Item Type" sub_items={["Assault Rifle", "Class Mod", "Grenade Mod", "Pistol", "Shield", "Shotgun", "Sniper Rifle", "Submachine Gun", "Relic" , "Repeater Pistol", "Rocket Launcher"]} />
             </ul>

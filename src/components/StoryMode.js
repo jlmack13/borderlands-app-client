a8be5d9c484c16a682b81048aa821fc50422bf69
jlmack13 <jennimacklin@gmail.com>
2018-07-03 11:04:55 -0400

    created story mode container and rendered in story mode component. set up api call in container for filtered list of loot

--- src/components/StoryMode.js
+++ src/components/StoryMode.js
@@ -1,15 +1,11 @@
 import React from 'react';
+import StoryModeContainer from '../containers/StoryModeContainer';
 
 const StoryMode = () => {
     return (
         <div className="story-mode">
-            <h1>Welcome to Story Mode!</h1>
-            <h2>Want to do a legend run playthrough? This is the place for you!</h2>
-            <p>This actually won't be this simple. I'll put the StoryModeContainer here. 
-                And hey look. It's another container component. Might not even need to filter 
-                by manufacturer or type after all. At least not to meet the requirements. 
-                Which is a good place to start and then build up from. Anyways Story Mode, coming soon!
-            </p>
+            <h1>Story Mode</h1>
+            <StoryModeContainer />
         </div>
     )
 }
---
+++ src/containers/StoryModeContainer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import LootList from '../components/LootList';
+
+const API_URL = process.env.REACT_APP_API_URL;
+
+class StoryModeContainer extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            loot: []
+        };
+    }
+
+    componentDidMount() {
+        fetch(`${API_URL}storymode`)
+            .then(response => response.json())
+            .then(loot => this.setState({ loot }))
+    }
+
+
+    render() {
+        return (
+            <div className="loot-container">
+                <LootList lootList={this.state.loot} />
+            </div>  
+        )
+    }
+}    
+
+export default StoryModeContainer;

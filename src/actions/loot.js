383d71a26baec528626069c0c9c7583be37b293f
jlmack13 <jennimacklin@gmail.com>
2018-07-06 15:01:44 -0400

    added fetch to create loot action

--- src/actions/loot.js
+++ src/actions/loot.js
@@ -1,4 +1,4 @@
-import { resetLootForm } from './looForm';
+import { resetLootForm } from './lootForm';
 const API_URL = process.env.REACT_APP_API_URL;
 
 // *= Action Creators **
@@ -23,5 +23,23 @@ export const getLoot = () => {
             .then(response => response.json())
             .then(loot => dispatch(setLoot(loot)))
             .catch( error => console.log(error))
+    }
+}
+
+export const createLoot = loot => {
+    return dispatch => {
+        return  fetch(`${API_URL}loots`, {
+             method: 'POST',
+             body: JSON.stringify(loot),
+             headers: {
+               'Content-Type': 'application/json',
+             }
+           })
+            .then(response => response.json())
+            .then(loot => {
+                dispatch(addLoot(loot))
+                dispatch(resetLootForm())
+            })
+            .catch(error => console.log(error))
     }
 }
